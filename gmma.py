3cc868ae72f5b8fff9feccf3b4689fa1143b2a00
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2024-05-05 17:22:24 +0500

    Update gmma.py

--- gmma.py
+++ gmma.py
@@ -11,6 +11,9 @@ def ema (self, array, period ):
     
     return ema 
 
+
+
+
 def gmma(close, **kwargs):
 
     gmma = np.empty((len(close), len(kwargs.keys()) ))
@@ -24,3 +27,25 @@ def gmma(close, **kwargs):
 gmma(close , ema1=3 ,ema2=5 ,ema3=8 ,ema4=10 ,ema5=12 ,ema6=15 ,ema7=30 ,ema8=35 ,ema9=40 ,ema10=45 ,ema11=50 ,ema12=60 )
 
 """
+
+def ema(price, period):
+
+        price = np.array(price)
+        alpha = 2 / (period + 1.0)
+        alpha_reverse = 1 - alpha
+        data_length = len(price)
+
+        power_factors = alpha_reverse ** (np.arange(data_length + 1))
+        initial_offset = price[0] * power_factors[1:]
+
+        scale_factors = 1 / power_factors[:-1]
+
+        weight_factor = alpha * alpha_reverse ** (data_length - 1)
+
+        weighted_price_data = price * weight_factor * scale_factors
+        cumulative_sums = weighted_price_data.cumsum()
+        ema_values = initial_offset + cumulative_sums * scale_factors[::-1]
+
+        return ema_values
+
+
